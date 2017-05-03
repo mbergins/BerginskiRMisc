84d0867ff593613a3b55b4b72d6f1f30b6a955f0
HoffmanLab <bhoffmanlab@gmail.com>
2017-05-03 10:55:30 -0400

    adding option to add file count as a column

--- R/data_misc.R
+++ R/data_misc.R
@@ -6,17 +6,27 @@
 #'
 #' This function uses Sys.glob to search for files matching a given string and then cycles through those files, reading them in as CSVs. This function assumes that all your CSV files are the same format or at least can be 'rbind'ed together.
 #' @param search_str string to pass to Sys.glob for the search
+#' @param add_file_num Optional: add an additional column (file_num) to indicate the count in the list of files being imported
 #' @keywords CSV
 #' @export
 #' @examples
 #' all_CSVs = read.csv.all('directory/*/*.csv')
 
-read.csv.all <- function(search_str) {
+read.csv.all <- function(search_str, add_file_num = F) {
 	csv_files = Sys.glob(search_str);
 
 	full_set = c();
+	file_count = 0;
 	for (this_file in csv_files) {
-		try({full_set = rbind(full_set,read.csv(this_file))})
+		file_count = file_count + 1;
+		try({
+			this_set = read.csv(this_file);
+			if(add_file_num) {
+				this_set = mutate(this_set, file_num = file_count);
+			}	
+			full_set = rbind(full_set,this_set);
+			
+		})
 	}
 	return(full_set);
 }

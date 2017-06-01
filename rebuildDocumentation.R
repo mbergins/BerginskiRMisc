71493fa8a79f543f3e1b1efdfe777885ca8787a8
HoffmanLab <bhoffmanlab@gmail.com>
2017-06-01 15:03:50 -0400

    removing unneeded quote marks

--- rebuildDocumentation.R
+++ rebuildDocumentation.R
@@ -3,6 +3,6 @@
 # Working from Hilary Parker's blog on personal R pacakges
 #  -http://hilaryparker.com/2014/04/29/writing-an-r-package-from-scratch/
 
-library('devtools')
+library(devtools)
 library(roxygen2)
 document()
